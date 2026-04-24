288fa0e94564f73f60b0121e66bf2ca8276af2fc
Jennifer Perez <strawberries2254@gmail.com>
2026-04-24 11:46:32 -0400

    trrying to add a dropdown menu for user icon, need to test online

--- front_end/scripts/utils.js
+++ front_end/scripts/utils.js
@@ -7,20 +7,21 @@ function isLoggedIn()
     }
 }
 
-function logout() 
-{
-    localStorage.removeItem("googleID");
-    localStorage.removeItem("userName");
-    window.location.href = "/index.html";
-}
-
 function displayProfileIcon()
 {
     if (isLoggedIn()) {
         document.getElementById("profile").style.display = "block";
+        document.getElementById("profile-dropdown").style.display = "block";
         const profilePicture = localStorage.getItem("picture");
         document.getElementById("profileImg").src = profilePicture;
     } else {
         document.getElementById("login").style.display = "block";
     }
+}
+
+function logout() 
+{
+    localStorage.removeItem("googleID");
+    localStorage.removeItem("userName");
+    window.location.href = "/index.html";
 }
