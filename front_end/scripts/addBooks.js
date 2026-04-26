5682f84b6bebbcdb59e3668f90ab50583c9c8451
Jake Kerrigan <jakekerrigan@Jakes-MacBook-Pro.local>
2026-04-26 16:11:32 -0400

    refactored exsiting js code, trying to get average published date working on heroku as it only works on local host currently?

--- front_end/scripts/addBooks.js
+++ front_end/scripts/addBooks.js
@@ -4,20 +4,6 @@ const params = new URLSearchParams(window.location.search);
 const googleID = params.get("userId");
 
 
-async function topSubject(isbn){
-    const url = `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&format=json&jscmd=data`;
-    const res = await fetch(url);
-    const data = await res.json();
-    const book = data[`ISBN:${isbn}`];
-    if(!book.subjects){
-        return [];
-    }
-    const top_genere = subjects.splice(0,5).map((element, index, array) => {
-        return element.name;
-      });
-    return top_genere;
-}
-
 
 async function addBook(book) {
 
@@ -55,45 +41,3 @@ async function addBook(book) {
     }
 }
 
-async function loadShelf() {
-    totalYear = 0;
-    const res = await fetch(`/api/shelf/${googleID}`);
-    const books = await res.json();
-
-    const list = document.getElementById("shelfList");
-    list.innerHTML = "";
-
-    books.forEach(book => {
-        const li = document.createElement("li");
-        li.innerText = `${book.title} (${book.author})`;
-        totalYear = totalYear + parseInt(book.image);
-
-        // remove button
-        const btn = document.createElement("button");
-        btn.innerText = "Remove";
-        
-        btn.addEventListener("click", async () => {
-            // remove from DOM
-            li.remove();
-
-            fetch(`/api/shelf/${googleID}/${book.id}`, {
-                method: "DELETE"
-            });
-
-            });
-        
-        li.appendChild(btn)
-        list.appendChild(li);
-    });
-
-    averageYear = Math.round(totalYear / books.length);
-
-    if(averageYear > 0){
-        averagePublishYear = document.getElementById("averagePublishYear");
-        averagePublishYear.innerHTML = `Average published year of bookshelf: ${averageYear}`;
-    }
-
-}
-
-// load on page start
-loadShelf();
