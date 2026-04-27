dd2f31681eabaedc5f517c0ab5eedbce631e7c95
Jake Kerrigan <jakekerrigan@Jakes-MacBook-Pro.local>
2026-04-27 18:36:24 -0400

    api work

--- front_end/scripts/rec.js
+++ front_end/scripts/rec.js
@@ -12,13 +12,12 @@ async function searchBook(query) {
 
 
 async function getRecommendation(){
-    subject = await getSubject();
+    const subject = await getSubject();
     const API_Key = "e48849f6aa854973a9e1b2fab1a207b9";
     const url =  "https://api.bigbookapi.com/search-books?api-key=" + API_Key + "&query=" + subject;
     const res = await fetch(url);
     const data = await res.json();
-    const title = data.books[0].title;
-    console.log(data);
+    const title = data.books[0][0].title;
     const book = await searchBook(title);
     await addBook(book);
     window.location.href = `/details.html?isbn=${book.isbn}`
